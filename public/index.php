9a9c1b7ecfbc2b66cb5d934c1e392284ef382d92
Nick Ocean <nickocean1997@gmail.com>
2018-10-09 15:42:43 +0300

    8

--- public/index.php
+++ public/index.php
@@ -47,5 +47,5 @@ debug($resp);*/
 
 // GET method basic usage
 $crm = new OroRequest($url, $userName, $userApiKey);
-$response = $crm->get('/index.php/api/leads/', 8);
+$response = $crm->get('/index.php/api/contacts/', 8);
 debug($response);
