a03839d62f27199ed6c0665220bc6e5dd864f603
Nick Ocean <nickocean1997@gmail.com>
2018-10-09 16:08:47 +0300

    type

--- src/ContactsAttributes.php
+++ src/ContactsAttributes.php
@@ -5,25 +5,25 @@ namespace Src;
 
 class ContactsAttributes extends Attributes {
 
-	public $gender = null;
-	public $birthday = null;
-	public $description = null;
-	public $fax = null;
-	public $skype = null;
-	public $facebook = null;
-	public $googlePlus = null;
+	public $gender;
+	public $birthday;
+	public $description;
+	public $fax;
+	public $skype;
+	public $facebook;
+	public $googlePlus;
 
 	public function __construct(
-		$name, $firstName, $lastName, $emails, $phones, $gender, $birthday, $description, $fax, $skype, $facebook, $googlePlus
+		$name, $firstName, $lastName, $emails, $phones
 	) {
 		parent::__construct( $name, $firstName, $lastName, $emails, $phones );
 
-		$this->gender = $gender;
+		/*$this->gender = $gender;
 		$this->birthday = $birthday;
 		$this->description = $description;
 		$this->fax = $fax;
 		$this->skype = $skype;
 		$this->facebook = $facebook;
-		$this->googlePlus = $googlePlus;
+		$this->googlePlus = $googlePlus;*/
 	}
 }
