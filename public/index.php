6e88f46c0fda6f93bfa24a727ff9b0c0bfd379ab
Nick Ocean <nickocean1997@gmail.com>
2018-10-11 10:51:07 +0300

    dfd

--- public/index.php
+++ public/index.php
@@ -63,7 +63,7 @@ debug($resp);*/
 
  // PUT lead with id = 118
 
-/*$attributes =new ContactsAttributes(
+$attributes =new ContactsAttributes(
 	'firstName_'.rand(1000, 2000),
 	'lastName_'.rand(1000, 2000),
 	new EmailsEntities(rand(100,200).'Email@gmail.com'),
@@ -72,7 +72,7 @@ debug($resp);*/
 $relationships = new Relationships();
 $relationships->addOwner('1');
 $relationships->addOrganization('1');
-$contact = new NewEntities( 'contacts', $attributes, $relationships);*/
+$contact = new NewEntities( 'contacts', $attributes, $relationships);
 
 
 $crm = new OroRequest($url, $userName ,$userApiKey);
